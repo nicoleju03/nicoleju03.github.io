a817f481997b426044c316d4d65f1ea84b02048d
Junwon Choi <cjunwon.tech@gmail.com>
2023-05-30 01:27:38 -0700

    Add pop-up icons and format message

--- src/components/Map/Icons.js
+++ src/components/Map/Icons.js
@@ -1,22 +1,141 @@
-import L from 'leaflet';
+import { Icon } from 'leaflet';
 import basketball_icon from './Images/basketball.png';
 import bruincard_icon from './Images/bruincard.png';
 import camera_icon from './Images/camera.png';
+import flag_pin_icon from './Images/flag-pin.png';
+import football_icon from './Images/football.png';
+import gavel_icon from './Images/gavel.png';
+import house_icon from './Images/house.png';
+import magazine_icon from './Images/magazine.png';
+import microphone_icon from './Images/microphone.png';
+import money_icon from './Images/money.png';
+import police_siren_icon from './Images/police-siren.png';
+import powell_cat_icon from './Images/powell-cat.png';
+import robot_icon from './Images/robot.png';
+import royce_hall_icon from './Images/royce-hall.png';
+import soccer_ball_icon from './Images/soccer-ball.png';
+import speakerphone_icon from './Images/speakerphone.png';
+import uaw_sign_icon from './Images/uaw-sign.png';
+import volleyball_icon from './Images/volleyball.png';
+import voting_box_icon from './Images/voting-box.png';
+import wifi_icon from './Images/wifi.png';
 
-export const basketball = L.icon({
+export const basketball = new Icon({
   iconUrl: basketball_icon,
-  iconSize: [32, 32],
-  iconAnchor: [16, 32],
+  iconSize:   [40, 40],
+  iconAnchor: [20, 20],
 });
 
-export const bruincard = L.icon({
+export const bruincard = new Icon({
   iconUrl: bruincard_icon,
-  iconSize: [32, 32],
-  iconAnchor: [16, 32],
+  iconSize:   [40, 40],
+  iconAnchor: [20, 20],
 });
 
-export const camera = L.icon({
+export const camera = new Icon({
   iconUrl: camera_icon,
-  iconSize: [32, 32],
-  iconAnchor: [16, 32],
+  iconSize:   [40, 40],
+  iconAnchor: [20, 20],
+});
+
+export const flag_pin = new Icon({
+	  iconUrl: flag_pin_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const football = new Icon({
+	  iconUrl: football_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const gavel = new Icon({
+	  iconUrl: gavel_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const house = new Icon({
+	  iconUrl: house_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const magazine = new Icon({
+	  iconUrl: magazine_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const microphone = new Icon({
+	  iconUrl: microphone_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const money = new Icon({
+	  iconUrl: money_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const police_siren = new Icon({
+	  iconUrl: police_siren_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const powell_cat = new Icon({
+	  iconUrl: powell_cat_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const robot = new Icon({
+	  iconUrl: robot_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const royce_hall = new Icon({
+	  iconUrl: royce_hall_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const soccer_ball = new Icon({
+	  iconUrl: soccer_ball_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const speakerphone = new Icon({
+	  iconUrl: speakerphone_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const uaw_sign = new Icon({
+	  iconUrl: uaw_sign_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const volleyball = new Icon({
+	  iconUrl: volleyball_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const voting_box = new Icon({
+	  iconUrl: voting_box_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
+});
+
+export const wifi = new Icon({
+	  iconUrl: wifi_icon,
+	  iconSize:   [40, 40],
+	  iconAnchor: [20, 20],
 });
--- src/components/Map/Interactive.js
+++ src/components/Map/Interactive.js
@@ -3,33 +3,143 @@ import { CRS, LatLngBounds } from 'leaflet';
 import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import style from './Interactive.css';
-import { basketball, bruincard, camera } from './Icons';
+import { basketball, bruincard, camera, flag_pin, football, gavel, house, magazine, microphone, money, police_siren, powell_cat, robot, royce_hall, soccer_ball, speakerphone, uaw_sign, volleyball, voting_box, wifi } from './Icons';
 import magnifyingGlass from './Images/magnifying-glass.png';
 
 function MyMap() {
-  const bounds = new LatLngBounds(
-    [34.0689, -118.4452], // Southwestern coordinate
-    [34.0789, -118.4352] // Northeastern coordinate
-  );
+    const bounds = new LatLngBounds(
+        [35.06, -220.50], // Southwestern coordinate (adjusted longitude)
+        [35.20, -220.36] // Northeastern coordinate (adjusted longitude)
+    );
 
   const markerPositions = [
     {
-      positionX: 0.2, // X coordinate relative to the image width (0.0 to 1.0)
-      positionY: 0.5, // Y coordinate relative to the image height (0.0 to 1.0)
-      icon: basketball,
-      popupMessage: 'Basketball Marker',
+        positionX: 0.2, // X coordinate relative to the image width (0.0 to 1.0)
+        positionY: 0.5, // Y coordinate relative to the image height (0.0 to 1.0)
+        icon: basketball,
+        popupMessage: (
+            <div>
+              <h3>March Madness</h3>
+              <p>With both UCLA women's and men's basketball teams clinching their tickets to the NCAA Tournament as top seeds, the two teams showed great promise in the end-of-year tournament. However, after hard-fought battles, both teams fell in the round of 16.</p>
+              {/* Insert image from pop-up images folder */}
+                <br></br>
+              <p style={{ fontSize: 'smaller' }}>Read more about <a href="https://features.dailybruin.com/2023/march-madness-2023/#men">UCLA basketball's journey to the tournament</a>.</p>
+            </div>
+          ),
     },
     {
-      positionX: 0.8, // X coordinate relative to the image width (0.0 to 1.0)
-      positionY: 0.3, // Y coordinate relative to the image height (0.0 to 1.0)
-      icon: bruincard,
-      popupMessage: 'Bruincard Marker',
+        positionX: 0.65,  
+        positionY: 0.52,  
+        icon: bruincard,
+        popupMessage: 'Bruincard Marker',
     },
     {
-      positionX: 0.6, // X coordinate relative to the image width (0.0 to 1.0)
-      positionY: 0.7, // Y coordinate relative to the image height (0.0 to 1.0)
-      icon: camera,
-      popupMessage: 'Camera Marker',
+        positionX: 0.6,  
+        positionY: 0.7,  
+        icon: camera,
+        popupMessage: 'Camera Marker',
+    },
+    {
+        positionX: 0.4,  
+        positionY: 0.4,  
+        icon: flag_pin,
+        popupMessage: 'Flag Pin Marker',
+    },
+    {
+        positionX: 0.55,  
+        positionY: 0.5,  
+        icon: football,
+        popupMessage: 'Football Marker',
+    },
+    // {
+    //     positionX: 0.6,  
+    //     positionY: 0.6,  
+    //     icon: gavel,
+    //     popupMessage: 'Gavel Marker',
+    // },
+    {
+        positionX: 0.65,  
+        positionY: 0.65,  
+        icon: house,
+        popupMessage: 'House Marker',
+    },
+    {
+        positionX: 0.33,  
+        positionY: 0.7,  
+        icon: magazine,
+        popupMessage: 'Magazine Marker',
+    },
+    {
+        positionX: 0.5,  
+        positionY: 0.38,  
+        icon: microphone,
+        popupMessage: 'Microphone Marker',
+    },
+    {
+        positionX: 0.4,  
+        positionY: 0.4,  
+        icon: money,
+        popupMessage: 'Money Marker',
+    },
+    {
+        positionX: 0.53,  
+        positionY: 0.68,  
+        icon: police_siren,
+        popupMessage: 'Police Siren Marker',
+    },
+    {
+        positionX: 0.3,  
+        positionY: 0.4,  
+        icon: powell_cat,
+        popupMessage: 'Powell Cat Marker',
+    },
+    {
+        positionX: 0.4,  
+        positionY: 0.5,  
+        icon: robot,
+        popupMessage: 'Robot Marker',
+    },
+    {
+        positionX: 0.5,  
+        positionY: 0.6,  
+        icon: royce_hall,
+        popupMessage: 'Royce Hall Marker',
+    },
+    {
+        positionX: 0.57,  
+        positionY: 0.78,  
+        icon: soccer_ball,
+        popupMessage: 'Soccer Ball Marker',
+    },
+    {
+        positionX: 0.45,  
+        positionY: 0.75,  
+        icon: speakerphone,
+        popupMessage: 'Speakerphone Marker',
+    },
+    {
+        positionX: 0.3,  
+        positionY: 0.52,  
+        icon: uaw_sign,
+        popupMessage: 'UAW Sign Marker',
+    },
+    {
+        positionX: 0.35,  
+        positionY: 0.6,  
+        icon: volleyball,
+        popupMessage: 'Volleyball Marker',
+    },
+    {
+        positionX: 0.225,  
+        positionY: 0.6,  
+        icon: voting_box,
+        popupMessage: 'Voting Box Marker',
+    },
+    {
+        positionX: 0.3,  
+        positionY: 0.8,  
+        icon: wifi,
+        popupMessage: 'Wifi Marker',
     },
   ];
 
@@ -40,7 +150,7 @@ function MyMap() {
 
     return (
       <Marker position={markerPosition} icon={icon}>
-        <Popup>{popupMessage}</Popup>
+        <Popup position={markerPosition}>{popupMessage}</Popup>
       </Marker>
     );
   });
@@ -52,6 +162,8 @@ function MyMap() {
         bounds={bounds}
         style={{ height: '100%', width: '100%' }}
         container="map-container"
+        scrollWheelZoom={false}
+        // zoomControl={true}
       >
         <ImageOverlay url={magnifyingGlass} bounds={bounds} />
         {markers}
