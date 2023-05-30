8d538f8845f67c4cda6c15068296af7bf9b476dd
Junwon Choi <cjunwon.tech@gmail.com>
2023-05-29 22:19:44 -0700

    Test pop-up

--- src/components/Map/Interactive.js
+++ src/components/Map/Interactive.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CRS, LatLngBounds } from 'leaflet';
-import { MapContainer, ImageOverlay, Marker } from 'react-leaflet';
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import style from './Interactive.css';
 
@@ -12,6 +12,17 @@ function MyMap() {
     [34.0789, -118.4352] // Northeastern coordinate
   );
 
+  const imageWidth = 4500; // Width of the magnifying glass image in pixels
+  const imageHeight = 4500; // Height of the magnifying glass image in pixels
+
+  const markerPositionX = 0.5; // Fractional X coordinate of the marker (0.0 to 1.0)
+  const markerPositionY = 0.5; // Fractional Y coordinate of the marker (0.0 to 1.0)
+
+  const markerLatitude = bounds.getSouth() + (markerPositionY * (bounds.getNorth() - bounds.getSouth()));
+  const markerLongitude = bounds.getWest() + (markerPositionX * (bounds.getEast() - bounds.getWest()));
+
+  const markerPosition = [markerLatitude, markerLongitude];
+
   return (
     <div id="map-container" style={{ height: '600px', width: '100%' }}>
       <MapContainer
@@ -21,7 +32,11 @@ function MyMap() {
         container="map-container"
       >
         <ImageOverlay url={magnifyingGlass} bounds={bounds} />
-        <Marker position={[34.0689, -118.4452]} />
+        <Marker position={markerPosition}>
+            <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
